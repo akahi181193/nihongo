c5d8ca180c2ba40d99cc6991f922b1408c82369e
dat <datnn1806@gmail.com>
2021-05-21 00:05:52 +0900

    del-script

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -201,8 +201,8 @@
     @endsection
 
     @section('scripts')
-    <!-- <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap4.min.js"></script>
+    
+<!--     
     <script type="text/javascript">
     $(document).ready( function () {
      var table =$('#').DataTable();
@@ -222,7 +222,7 @@
         $('#editform').attr('action','/employee/'+data[0]);
         $('#editform').modal('show');
     });
-    } );
+    } ); -->
     
     </script> -->
         <script>
