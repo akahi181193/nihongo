d0fe5090cbbf5b4c109957e2f7072914907c430a
quocthanh <sieusao190@gmail.com>
2021-06-26 18:02:13 +0900

    avatar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -81,6 +81,7 @@
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
+                                    <img width="responssive" style="border-radius: 90px;height: 30px;width: 30px;" src="{{ asset('storage/avatar/' . Auth::user()->avatar) }}">
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
